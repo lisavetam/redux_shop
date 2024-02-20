a17af18c58f41d5f54e6584d4a0bb27958d18d8b
elizaveta schlegel <elizaveta.schlegel@gmail.com>
2024-02-20 14:24:49 +0100

    modified all files

---
+++ src/Components/Filter/AllCategories.js
@@ -0,0 +1,16 @@
+import Filter from "./Filter";
+
+
+const AllCatagories =() => {
+    return(
+        <div>
+            <h1>What kind of food do you like? </h1>
+            {['SEAFOOD','APPETIZERS', 'ITALIAN','SALADS','ALL'].
+            map(category =>  <Filter category={category}/>)
+    }
+        </div>
+    
+    )
+}
+
+export default AllCatagories;
